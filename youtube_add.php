75ff1ac92afa0af6caebd081fcfe49f1da8745c7
JohnSmithWithHuruhi <jason82211@yahoo.com.tw>
2015-04-28 00:46:05 +0800

    Update youtube_add.php

--- youtube_add.php
+++ youtube_add.php
@@ -12,10 +12,6 @@ $code_get = $_GET["code"];
 
 $code_md5=md5($current. "??????");
 
-//echo "</br>"."add vid:" . $current."</br>";
-//echo "</br>"."input code =".$code_get."</br>";
-//echo "</br>"."MD5('".$current."??????')=".$code_md5."</br></br></br>";
-
 if ($code_get== $code_md5)
 {
 file_put_contents($file, $current);
